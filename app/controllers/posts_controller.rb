2b2389866d12d542918195fc81ef3e4f106d0471
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2015-10-19 06:55:13 +0000

    add new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+
 end
